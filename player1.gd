135144488fe5420e82354c9cd1070b33f1340a8b
Jnight1104 <jnight1104@gmail.com>
2024-05-29 10:33:50 +1200

    making code to calculate ship velocity

--- player1.gd
+++ player1.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
 var speed = 10000
-var target_rotation = 0.0
+var acceleration = 2
+var shipvelocity = 0
+var friction = 0
+var target_rotation = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame.	
 func _process(delta):
+	#friction calculation based off velocity
+	friction = pow(1.2, (shipvelocity - 8))
+	#moves ship forward constantly based off shipvelocity
+	velocity = transform.x * 10 * ((shipvelocity * delta)-(friction * delta))
 	# Movement input
 	if Input.is_action_pressed("ui_up"):
-		velocity = transform.x * speed * delta
+		shipvelocity += acceleration
 	elif Input.is_action_pressed("ui_down"):
-		velocity = -transform.x * speed * delta
+		shipvelocity -= acceleration
 	else:
 		velocity = lerp(velocity, Vector2(0.0, 0.0), 0.005)
 
@@ -27,7 +34,7 @@ func _process(delta):
 	rotation = lerp_angle(rotation, target_rotation, 0.1)
 
 	# Print the current rotation in degrees
-	print(rad_to_deg(rotation))
-
+	#print(rad_to_deg(rotation))
+	print(shipvelocity)
 	# Apply velocity and move the character
 	move_and_slide()
