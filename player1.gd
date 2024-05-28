170a172f7c59fd8393e211914ca670ad31ed4f2e
Jnight1104 <jnight1104@gmail.com>
2024-05-28 15:06:22 +1200

    added acceleration physics for forward movement

--- player1.gd
+++ player1.gd
@@ -1,6 +1,6 @@
-extends Area2D
+extends CharacterBody2D
 
-var speed = 400
+var speed = 10000
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,12 +8,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
-		position += transform.x * speed * delta
+		velocity = transform.x * speed * delta
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		velocity = -transform.x * speed * delta
 	if Input.is_action_pressed("ui_left"):
 		self.rotate(-0.1)
 	if Input.is_action_pressed("ui_right"):
 		self.rotate(0.1)
+	if not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
+		velocity = lerp(velocity, Vector2(0.0, 0.0), 0.005)
+		
+	print(rad_to_deg(rotation))
+	
+	move_and_slide()
