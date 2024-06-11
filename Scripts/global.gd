1407abbcd4ab59dcf8a5ff0f4b29fb30951d19ef
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-06-12 10:45:40 +1200

    fixes

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var p1_score = 0
 var p2_score = 0
+var p1_location = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,7 +2,13 @@
 
 extends Node2D
 
+@onready var global = get_node("/root/Global")
 @onready var camera = get_node("Camera2D")
 
 func _ready():
 	CameraLimits.set_limits(camera.limit_left, camera.limit_right, camera.limit_top, camera.limit_bottom)
+
+
+func _process(delta):
+	$P1_score.text = ("P1: " + str(global.p1_score))
+	$P2_score.text = ("P2: " + str(global.p2_score))
