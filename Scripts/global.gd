adf44696e5d07312011f65ba05e1bea57d01773b
Jnight1104 <jnight1104@gmail.com>
2024-06-14 08:44:19 +1200

    Shifted node ordering

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -2,8 +2,8 @@ extends Node
 
 var p1_score = 0
 var p2_score = 0
-var p1_location = 0
-var p2_location = 0
+var p1_location = Vector2(0,0)
+var p2_location = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
