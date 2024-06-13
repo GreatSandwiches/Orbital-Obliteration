407b0126705ca35534ea5d4261346bc1f2ab63f6
Jnight1104 <jnight1104@gmail.com>
2024-06-14 09:14:52 +1200

    Added styling and lifetime to p1 trail

--- Scripts/P1_trail.gd
+++ Scripts/P1_trail.gd
@@ -5,9 +5,15 @@ extends Line2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global_position = Vector2(0,0)
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = Vector2(0,0)
 	add_point(global.p1_location, -1)
+	
+	print(get_point_position(0))
+	if get_point_position(0) == Vector2(0,0):
+		remove_point(0)
+	if get_point_count() > 20:
+		remove_point(0)
