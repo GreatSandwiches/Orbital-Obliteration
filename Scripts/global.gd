dd4c3d183bf4f1d3382a571b2e889cb213f78736
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-06-14 14:11:06 +1200

    Adding global variables for health in preperation of changing health bar method

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,9 +1,13 @@
 extends Node
-
+# p1 variables
 var p1_score = 0
-var p2_score = 0
 var p1_location = Vector2(0,0)
+var p1_health = 5
+#p2 variables
+var p2_score = 0
 var p2_location = Vector2(0,0)
+var p2_health = 5
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
