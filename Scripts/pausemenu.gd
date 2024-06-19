c58d7cb85947e5b6c6216a852bd9129191294af3
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-06-19 18:13:01 +1200

    Adding a main menu button to pausemenu

--- Scripts/pausemenu.gd
+++ Scripts/pausemenu.gd
@@ -6,6 +6,7 @@ func _on_close_button_pressed():
 	get_tree().paused = false
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +15,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_menu_button_pressed():
+	hide()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/mainmenu.tscn")
