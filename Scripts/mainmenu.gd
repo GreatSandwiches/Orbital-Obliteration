8f1593dd53956034fb3aaa516f31e1f71861d73e
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-08-06 22:19:55 +1200

    Adding a loading screen

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -13,7 +13,7 @@ func _process(delta):
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+	get_tree().change_scene_to_file("res://Scenes/loadingscreen.tscn")
 	
 
 func _on_options_pressed():
