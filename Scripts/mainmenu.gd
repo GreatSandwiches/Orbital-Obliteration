7333afb24b49c9154b483c58d9fba01092906036
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-10-16 16:40:04 +1300

    Updating code to meet conventions

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -2,21 +2,23 @@ extends Control
 @onready var global = get_node("/root/Global")
 @onready var menuparticles = get_node("/root/MenuParticles")
 @onready var mute_button: Button = $MuteButton
-
+var low_quality = 2
+var medium_quality = 1
+var high_quality = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global.ismainmenu = true
-	menuparticles.show() #ach this doesnt work yet
+	menuparticles.show() 
 	mute_button.text = "Mute"
 	
+	# Checking if settings are loaded or not
 	if global.settings_loaded == false:
 		_load_settings()
 		global.settings_loaded = true
 	
-	
-	
+	# Checking the state of the "mute" button when loading
 	if global.is_muted:
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
 		mute_button.text = "Unmute" 
@@ -26,26 +28,28 @@ func _ready():
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 		mute_button.text = "Mute"
 	
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
+# Changes scene to the mode select screen
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/modeselect.tscn")
 	
-
+	
+# Opens options menu when button pressed
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Scenes/settingsmenu.tscn")
 	
-
+	
+# Quits the game when button pressed
 func _on_quit_pressed():
 	get_tree().quit()
 
 
-
+# Function handling the different states of the mute button
 func _on_mute_button_pressed():
 	global.is_muted = !global.is_muted  # Toggle mute state
 	if global.is_muted:
@@ -57,12 +61,11 @@ func _on_mute_button_pressed():
 		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 		mute_button.text = "Mute"
 		
-		
-		
-# load the settings from a file when the project starts
+				
+# Loads the graphics settings from a file when the project starts
 func _load_settings():
 	var file = FileAccess.open("user://video_settings.save", FileAccess.READ)
-	if file:  # Check if the file exists
+	if file:  # Checks if the file exists
 		var quality = file.get_32()
 		var fullscreen_str = file.get_line()
 		file.close()
@@ -73,15 +76,15 @@ func _load_settings():
 		# Set the MSAA based on the saved quality
 		match quality:
 			0:
-				get_viewport().msaa_2d = Viewport.MSAA_8X
-				global.quality = 0
+				get_viewport().msaa_2d = Viewport.MSAA_8X # High quality
+				global.quality = high_quality
 			1:
-				get_viewport().msaa_2d = Viewport.MSAA_2X
-				global.quality = 1
+				get_viewport().msaa_2d = Viewport.MSAA_2X # Medium quality
+				global.quality = medium_quality
 			2:
-				get_viewport().msaa_2d = Viewport.MSAA_DISABLED
-				global.quality = 2
-
+				get_viewport().msaa_2d = Viewport.MSAA_DISABLED # Low quality
+				global.quality = low_quality
+	
 		# Set the fullscreen mode based on the saved value
 		if fullscreen:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
@@ -90,16 +93,19 @@ func _load_settings():
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			global.fullscreen = false
 			
-		
+	else: # Settings will be reset to default if no file is found
+		print("Failure fetching file")
+			
+	# Loads the audio settings from a file when the game starts	
 	if FileAccess.file_exists("user://audio_settings.save"):
 		
 		var audiofile = FileAccess.open("user://audio_settings.save", FileAccess.READ)
 		if audiofile:  # Check if the file exists
-			var volume = audiofile.get_var()
+			var volume = audiofile.get_var() # Accessing file
 			print(volume)
 			audiofile.close()
 		
-			AudioServer.set_bus_volume_db(0, volume/5)
+			AudioServer.set_bus_volume_db(0, volume/5) # Setting the game volume
 			global.volume = volume
 	else: 
 		print("audio file does not exist")
--- Scripts/player1.gd
+++ Scripts/player1.gd
@@ -26,23 +26,36 @@ var angle_list = []
 var max_health = 100
 var default_firerate = 0.3
 var default_coolingrate = 3
+var overheated = 10
+var rotation_amount = 0.075
+var regular_bullet_speed = 1
+var slowed_bullet_speed = 0.7
+var starting_position = Vector2(120,150)
+var mine_damage = 30
+var enhanced_shotgun_size = Vector2(1.4,1.4)
+var big_bullet_size = Vector2(2,2)
+var shotgun_bullet_size = Vector2(0.7,0.7)
+
 signal shield_animation
 signal powered_up
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Shieldframes.stop()
 	$Shieldframes.set_frame_and_progress(0,0.0)
 	global.p1_health = max_health
 	global.p1_gunheat = 0
-	global.p1_firerate = 0.3
+	global.p1_firerate = default_firerate
 	global.p1_gundamage = base_gundamage
-	global.p1_coolingrate = 3
-	global.p1_bulletspeed = 1
-	global.p1_maxgunheat = 10
+	global.p1_coolingrate = default_coolingrate
+	global.p1_bulletspeed = regular_bullet_speed
+	global.p1_maxgunheat = overheated
 	$Timer.wait_time = shoot_cooldown
 	$Timer.one_shot = true
 	$Timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 	powered_up.connect(get_node("/root/Level/P1_UI")._powered_up)
+
 
 # Bullet dmg and knockback code
 func _hit(projectile, bullet_vel, damage):
@@ -64,43 +77,56 @@ func _hit(projectile, bullet_vel, damage):
 			shipvector += (bullet_vel * 0.0005 * projectile.get_scale())
 
 
+# Removes immunity when player shield runs out
 func _shield_down():
 	immunity = false
 
+
+# Applies shield effect when shield powerup is collected
 func _shield_powerup_collected():
 	$Shieldframes.stop()
 	$Shieldframes.set_frame_and_progress(0,0.0)
 	shield = true
 	immunity = true
 
+
+# Activates the missile when missile powerup is collected
 func _missile_powerup_collected():
 	missile = 1
 
+
+# Calculates damage taken and when to execute die() function
 func take_damage(amount):
 	global.p1_health -= amount
 	if global.p1_health <= 0:
 		die()
-		
+	
+	
+# Damages and calculates knockback when player hits a spacemine		
 func _mine_collision():
 	knockback = (position - global.spacemine_collision_pos_p1)
 	shipvector += knockback * 0.07
 	if immunity == false:
-		take_damage(30)
+		take_damage(mine_damage)
 	else:
 		$Shieldframes.play()
 		shield = false
 
+
+# Resetting and respawning player when they die
 func die():
 	global.p1_health = max_health
-	global.p2_score +=1
+	global.p2_score += 1
 	global.p1_gunheat = 0
-	position = Vector2(120, 150)
+	position = starting_position
 	$Shieldframes.stop()
 	$Shieldframes.set_frame_and_progress(0,0.0)
 	shield = true
 	immunity = true
 	shipvector = Vector2(0,0)
 	
+
+# Function to handle shooting of bullets	
 func _shoot(deviation, type):
 	var bullet = bullet_p1_scene.instantiate()
 	var missile = missile_p1_scene.instantiate()
@@ -108,20 +134,21 @@ func _shoot(deviation, type):
 	bullet.rotation = rotation + deviation
 	missile.position = $ProjectileSpawn.global_position
 	missile.rotation = rotation + deviation
+	
 	if big_bullet == true:
 		if shotgun == true:
-			bullet.set_scale(Vector2(1.4,1.4))
-			missile.set_scale(Vector2(1.4,1.4))
+			bullet.set_scale(enhanced_shotgun_size)
+			missile.set_scale(enhanced_shotgun_size)
 		else:
-			bullet.set_scale(Vector2(2,2))
-			missile.set_scale(Vector2(2,2))
-		global.p1_gundamage = base_gundamage * 2
-		global.p1_bulletspeed = 0.7
+			bullet.set_scale(big_bullet_size)
+			missile.set_scale(big_bullet_size)
+		global.p1_gundamage = base_gundamage * 2 # Doubling damage 
+		global.p1_bulletspeed = slowed_bullet_speed
 	else:
 		if shotgun == true:
-			bullet.set_scale(Vector2(0.7,0.7))
-			missile.set_scale(Vector2(0.7,0.7))
-		global.p1_bulletspeed = 1
+			bullet.set_scale(shotgun_bullet_size)
+			missile.set_scale(shotgun_bullet_size)
+		global.p1_bulletspeed = regular_bullet_speed
 		global.p1_gundamage = base_gundamage
 	if type == "missile":
 		get_parent().add_child(missile)
@@ -129,17 +156,20 @@ func _shoot(deviation, type):
 		get_parent().add_child(bullet)
 		print(global.p1_gundamage)
 	
+	
 func _shotgun_powerup_collected():
 	base_gundamage = 5
 	shotgun = true
 	$ShotgunTimer.start(10)
 	powered_up.emit()
 
+
 func _on_shotgun_timer_timeout():
 	base_gundamage = 20
 	shotgun = false
 
-	# Function to handle the rapid-fire power-up
+
+# Function to handle the rapid-fire power-up
 func _on_rapidfire_entered(area):
 	if area.has_meta("rapidfire"):
 		global.p1_firerate = 0.1
@@ -151,12 +181,14 @@ func _on_rapidfire_entered(area):
 		$RapidFireTimer.start(10)  # 10 seconds duration
 		powered_up.emit()
 
+
 # Function to reset the fire rate and cooling rate
 func _on_RapidFireTimer_timeout():
-	global.p1_firerate = default_firerate  # Reset to default 
-	global.p1_coolingrate = default_coolingrate # Reset to default 
+	global.p1_firerate = default_firerate   
+	global.p1_coolingrate = default_coolingrate  
 	print("Power-up ended")
 	print(global.p1_firerate)
+
 
 func _on_damagepowerup_entered(area):
 	if area.has_meta("damageincrease"):
@@ -164,17 +196,21 @@ func _on_damagepowerup_entered(area):
 		big_bullet = true
 		powered_up.emit()
 
+
 func _on_DamagePowerupTimer_timeout():
 	big_bullet = false
 
+
 func _on_timer_timeout():
 	can_shoot = true
+
 
 func _on_overheat_timer_timeout():
 	is_overheated = false
 	can_shoot = true
 
-func _process(delta):
+
+func _process(_delta):
 	if global.game_mode == 0:
 		visible = false
 		velocity = Vector2.ZERO
@@ -184,12 +220,13 @@ func _process(delta):
 	
 	global.p1_position = position
 	global.p1_velocity = velocity
+	
 	# Constrain the player's position within the camera limits
 	position.x = clamp(position.x, CameraLimits.limit_left, CameraLimits.limit_right)
 	position.y = clamp(position.y, CameraLimits.limit_top, CameraLimits.limit_bottom)
 
 	# Outputs ship velocity by adding acceleration subtracted by friction
-	shipvectorforward = Vector2(transform.x * acceleration * delta)
+	shipvectorforward = Vector2(transform.x * acceleration * _delta)
 	shipvector += shipvectorforward - shipvectorbackward
 	# Calculates friction vector
 	
@@ -202,12 +239,12 @@ func _process(delta):
 	if shipvector.y <= 0:
 		shipvectorbackward.y = 0.01 * (pow((shipvector.y - 1.2), 3) + 1.728)
 	
-	#current position identifier for use with trail
+	# Current position identifier for use with trail
 	global.p1_location = -10 * transform.x + position
 	
 	velocity = 400 * shipvector
 	
-	#smoke trail activation
+	# Smoke trail activation
 	if global.p1_health < 30:
 		$Area2D/SmokeTrail.emitting = true
 		
@@ -216,7 +253,7 @@ func _process(delta):
 	
 	# Heat cooldown functionality
 	if global.p1_gunheat > 0 and cancool:
-		global.p1_gunheat -= global.p1_coolingrate * delta
+		global.p1_gunheat -= global.p1_coolingrate * _delta
 		if global.p1_gunheat < 0:
 			global.p1_gunheat = 0
 	
@@ -233,14 +270,14 @@ func _process(delta):
 		acceleration = 0
 	# Rotation input
 	if Input.is_action_pressed("ui_a"):
-		target_rotation -= 0.075
+		target_rotation -= rotation_amount
 	elif Input.is_action_pressed("ui_d"):
-		target_rotation += 0.075
+		target_rotation += rotation_amount
 
 	# Smooth rotation using lerp_angle
 	rotation = lerp_angle(rotation, target_rotation, 1)
 	
-	# detecting for if player can shoot when key is pressed
+	# Detecting for if player can shoot when key is pressed
 	if Input.is_action_pressed("ui_shift") and can_shoot and not is_overheated:
 		if global.p1_gunheat < global.p1_maxgunheat:
 			# Code for generating shotgun bullets
@@ -249,8 +286,6 @@ func _process(delta):
 				if missile == 1:
 					angle_list = [(-2*PI/7), (-1*PI/7), 0, (1*PI/7), (2*PI/7)]
 				for deviation in angle_list:
-					#var rng = RandomNumberGenerator.new()
-					#global.p1_bulletspeed = 1 + rng.randf_range(-0.2, 0)
 					if missile == 1:
 						_shoot(deviation, "missile")
 						global.p1_gunheat += 2
@@ -268,7 +303,7 @@ func _process(delta):
 			$Timer.start(global.p1_firerate)
 		else:
 			is_overheated = true
-			global.p1_gunheat = 10
+			global.p1_gunheat = overheated
 			can_shoot = false
 			$OverheatTimer.start(1)
 			
