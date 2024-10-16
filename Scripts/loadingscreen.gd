23c4bacc04aa185368f7b54ab5322a786382db87
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-10-16 17:11:12 +1300

    Update loadingscreen.gd

--- Scripts/loadingscreen.gd
+++ Scripts/loadingscreen.gd
@@ -1,39 +1,46 @@
 extends Control
-@onready var global = get_node("/root/Global")
 
+# Variables
+@onready var global = get_node("/root/Global")
 @export var loading_bar : ProgressBar
 @export var percentage_label : Label
-
 var scene_path : String
 var progress : Array
 var update : float = 0.0
+const SMOOTHNESS = 0.2
+const LOADED = 1.0
 
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	scene_path = global.selected_level
-	ResourceLoader.load_threaded_request(scene_path)
+	ResourceLoader.load_threaded_request(scene_path) # Begin loading
 	
-
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	ResourceLoader.load_threaded_get_status(scene_path, progress)
+	ResourceLoader.load_threaded_get_status(scene_path, progress) # Updating loading status
 	
 	if progress[0] > update:
 		update = progress[0]
-		
-		
 	
-	if loading_bar.value >= 1.0:
-		if update >= 1.0:
+	# Detecting when finished loading - loads into level
+	if loading_bar.value >= LOADED:
+		if update >= LOADED:
 			global.ismainmenu = false
 			get_tree().change_scene_to_packed(
 				ResourceLoader.load_threaded_get(scene_path)
 			)
+			
+	# Updates the loading bar if it's value is less than the current update
 	if loading_bar.value < update:
 		loading_bar.value = lerp(loading_bar.value, update, delta)
-		
 	
-	loading_bar.value += delta * 0.2 * \
-	 	(0.5 if update >= 1.0 else clamp(0.9 - loading_bar.value, 0.0, 1.0))
-		
+	# Smoothing the progress of the loading bar
+	loading_bar.value += delta * SMOOTHNESS * \
+	 	(0.5 if update >= LOADED else clamp(0.9 - loading_bar.value, 0.0, 1.0))
+	
+	# Updating the percentage indicator label, converting to a percentage
 	percentage_label.text = str(int(update * 100.0)) + " %"
 	
 	
