63b2bd8e843b11d00333b916acc31fc0fdd65251
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-10-20 12:42:30 +1300

    updating code to meet conventions

--- Scripts/player1.gd
+++ Scripts/player1.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
+@onready var global = get_node("/root/Global")
 var speed = 10000
 var acceleration = 0
 var target_rotation = 0
-var shipvector = Vector2(0,0)
-var shipvectorforward = Vector2(0,0)
-var shipvectorbackward = Vector2(0,0)
+var ship_vector = Vector2(0,0)
+var ship_vector_forward = Vector2(0,0)
+var ship_vector_backward = Vector2(0,0)
 @export var bullet_p1_scene: PackedScene
 @export var missile_p1_scene: PackedScene
 var can_shoot = true
@@ -14,7 +15,6 @@ var score = 0
 var cancool = true
 var knockback = Vector2(0,0)
 var bullet_knockback = Vector2(0,0)
-@onready var global = get_node("/root/Global")
 var is_overheated = false
 var big_bullet = false
 var shotgun = false
@@ -35,6 +35,12 @@ var mine_damage = 30
 var enhanced_shotgun_size = Vector2(1.4,1.4)
 var big_bullet_size = Vector2(2,2)
 var shotgun_bullet_size = Vector2(0.7,0.7)
+var knockback_amount = 0.0005
+var missile_damage = 2.5
+var mine_knockback = 0.07
+var rapid_fire_rate = 0.1
+var rapid_cooling_rate = 8
+var low_health = 30
 
 signal shield_animation
 signal powered_up
@@ -44,6 +50,8 @@ signal powered_up
 func _ready():
 	$Shieldframes.stop()
 	$Shieldframes.set_frame_and_progress(0,0.0)
+	
+	# Setting variables to default
 	global.p1_health = max_health
 	global.p1_gunheat = 0
 	global.p1_firerate = default_firerate
@@ -65,16 +73,16 @@ func _hit(projectile, bullet_vel, damage):
 	if immunity == false:
 		if projectile.is_in_group("p2_bullet"):
 			take_damage(damage)
-			shipvector += (bullet_vel * 0.0005 * projectile.get_scale())
+			ship_vector += (bullet_vel * knockback_amount * projectile.get_scale())
 		if projectile.is_in_group("p2_missile"):
-			take_damage(damage * 2.5)
-			shipvector += (bullet_vel * 0.0005 * projectile.get_scale())
+			take_damage(damage * missile_damage)
+			ship_vector += (bullet_vel * knockback_amount * projectile.get_scale())
 		if projectile.is_in_group("enemy_bullet"):
 			take_damage(damage)
-			shipvector += (bullet_vel * 0.0005 * projectile.get_scale())
+			ship_vector += (bullet_vel * knockback_amount * projectile.get_scale())
 		if projectile.is_in_group("enemy_missile"):
-			take_damage(damage * 2.5)
-			shipvector += (bullet_vel * 0.0005 * projectile.get_scale())
+			take_damage(damage * missile_damage)
+			ship_vector += (bullet_vel * knockback_amount * projectile.get_scale())
 
 
 # Removes immunity when player shield runs out
@@ -92,7 +100,7 @@ func _shield_powerup_collected():
 
 # Activates the missile when missile powerup is collected
 func _missile_powerup_collected():
-	missile = 1
+	missile = 1 # Activates the missile (1 is active, 0 is inactive)
 
 
 # Calculates damage taken and when to execute die() function
@@ -105,7 +113,7 @@ func take_damage(amount):
 # Damages and calculates knockback when player hits a spacemine		
 func _mine_collision():
 	knockback = (position - global.spacemine_collision_pos_p1)
-	shipvector += knockback * 0.07
+	ship_vector += knockback * mine_knockback
 	if immunity == false:
 		take_damage(mine_damage)
 	else:
@@ -123,7 +131,7 @@ func die():
 	$Shieldframes.set_frame_and_progress(0,0.0)
 	shield = true
 	immunity = true
-	shipvector = Vector2(0,0)
+	ship_vector = Vector2(0,0)
 	
 
 # Function to handle shooting of bullets	
@@ -135,6 +143,7 @@ func _shoot(deviation, type):
 	missile.position = $ProjectileSpawn.global_position
 	missile.rotation = rotation + deviation
 	
+	# Handling of different powerup scenarios
 	if big_bullet == true:
 		if shotgun == true:
 			bullet.set_scale(enhanced_shotgun_size)
@@ -144,6 +153,7 @@ func _shoot(deviation, type):
 			missile.set_scale(big_bullet_size)
 		global.p1_gundamage = base_gundamage * 2 # Doubling damage 
 		global.p1_bulletspeed = slowed_bullet_speed
+		
 	else:
 		if shotgun == true:
 			bullet.set_scale(shotgun_bullet_size)
@@ -157,13 +167,15 @@ func _shoot(deviation, type):
 		print(global.p1_gundamage)
 	
 	
+# Function to activate the shotgun when powerup collected
 func _shotgun_powerup_collected():
 	base_gundamage = 5
 	shotgun = true
 	$ShotgunTimer.start(10)
 	powered_up.emit()
+	
 
-
+# Removes the effects of the shotgun when the powerup runs out
 func _on_shotgun_timer_timeout():
 	base_gundamage = 20
 	shotgun = false
@@ -172,8 +184,8 @@ func _on_shotgun_timer_timeout():
 # Function to handle the rapid-fire power-up
 func _on_rapidfire_entered(area):
 	if area.has_meta("rapidfire"):
-		global.p1_firerate = 0.1
-		global.p1_coolingrate = 8
+		global.p1_firerate = rapid_fire_rate
+		global.p1_coolingrate = rapid_cooling_rate
 		print("Power-up triggered")
 		print(global.p1_firerate)
 		
@@ -190,6 +202,7 @@ func _on_RapidFireTimer_timeout():
 	print(global.p1_firerate)
 
 
+# Function to activate the damage powerup
 func _on_damagepowerup_entered(area):
 	if area.has_meta("damageincrease"):
 		$DamageBoostTimer.start(10)
@@ -197,20 +210,26 @@ func _on_damagepowerup_entered(area):
 		powered_up.emit()
 
 
+# Deactivates the damage powerup when timer runs out
 func _on_DamagePowerupTimer_timeout():
 	big_bullet = false
 
 
+# Timer controlling firerate
 func _on_timer_timeout():
 	can_shoot = true
 
 
+# Function to allow player to shoot once gun is no longer overheated
 func _on_overheat_timer_timeout():
 	is_overheated = false
 	can_shoot = true
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	# If game mode is singleplayer, player1 will not show
 	if global.game_mode == 0:
 		visible = false
 		velocity = Vector2.ZERO
@@ -218,6 +237,7 @@ func _process(delta):
 		queue_free()
 		return
 	
+	# Updating global posiition and velocity constantly
 	global.p1_position = position
 	global.p1_velocity = velocity
 	
@@ -226,26 +246,25 @@ func _process(delta):
 	position.y = clamp(position.y, CameraLimits.limit_top, CameraLimits.limit_bottom)
 
 	# Outputs ship velocity by adding acceleration subtracted by friction
-	shipvectorforward = Vector2(transform.x * acceleration * delta)
-	shipvector += shipvectorforward - shipvectorbackward
-	# Calculates friction vector
+	ship_vector_forward = Vector2(transform.x * acceleration * delta)
+	ship_vector += ship_vector_forward - ship_vector_backward
 	
-	if shipvector.x >= 0:
-		shipvectorbackward.x = 0.01 * (pow((shipvector.x + 1.2), 3) - 1.728)
-	if shipvector.x <= 0:
-		shipvectorbackward.x = 0.01 * (pow((shipvector.x - 1.2), 3) + 1.728)
-	if shipvector.y >= 0:
-		shipvectorbackward.y = 0.01 * (pow((shipvector.y + 1.2), 3) - 1.728)
-	if shipvector.y <= 0:
-		shipvectorbackward.y = 0.01 * (pow((shipvector.y - 1.2), 3) + 1.728)
+	# Calculates friction vector
+	if ship_vector.x >= 0:
+		ship_vector_backward.x = 0.01 * (pow((ship_vector.x + 1.2), 3) - 1.728)
+	if ship_vector.x <= 0:
+		ship_vector_backward.x = 0.01 * (pow((ship_vector.x - 1.2), 3) + 1.728)
+	if ship_vector.y >= 0:
+		ship_vector_backward.y = 0.01 * (pow((ship_vector.y + 1.2), 3) - 1.728)
+	if ship_vector.y <= 0:
+		ship_vector_backward.y = 0.01 * (pow((ship_vector.y - 1.2), 3) + 1.728)
 	
 	# Current position identifier for use with trail
 	global.p1_location = -10 * transform.x + position
-	
-	velocity = 400 * shipvector
+	velocity = 400 * ship_vector
 	
 	# Smoke trail activation
-	if global.p1_health < 30:
+	if global.p1_health < low_health:
 		$Area2D/SmokeTrail.emitting = true
 		
 	else:
